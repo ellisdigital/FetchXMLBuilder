ac1702ef50b23336f9b4aa70144a7373ebde06f2
ellisdigital <netvbonline@gmail.com>
2019-11-01 11:36:38 +0000

    Testing

--- FetchXmlBuilder/Forms/Settings.Designer.cs
+++ FetchXmlBuilder/Forms/Settings.Designer.cs
@@ -534,7 +534,7 @@
             this.btnOK.Name = "btnOK";
             this.btnOK.Size = new System.Drawing.Size(75, 23);
             this.btnOK.TabIndex = 0;
-            this.btnOK.Text = "&OK";
+            this.btnOK.Text = "OK";
             this.btnOK.UseVisualStyleBackColor = true;
             this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
             // 
